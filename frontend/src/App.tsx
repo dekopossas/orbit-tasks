afbd60fe0c859de7ac814f77cfe9c210da914ad1
deko possas <andrepossaster@gmail.com>
2021-07-22 12:01:55 -0300

    super button render test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@seidor-cloud-produtos/4tax-react-components';
 
 function App() {
   return (
     <div className="App">
       Hello world
+      <Button>
+        super button!
+      </Button>
     </div>
   );
 }
