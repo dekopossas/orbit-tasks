5dd52472fd9e917575551502831d29a97a99f732
deko possas <andrepossaster@gmail.com>
2021-07-26 14:12:55 -0300

    super button test

--- frontend/src/views/home/index.tsx
+++ frontend/src/views/home/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@seidor-cloud-produtos/4tax-react-components'
 import React from 'react'
 
 function Home() {
   return (
     <div>
       Home
+      <Button>Super Button</Button>
     </div>
   )
 }
