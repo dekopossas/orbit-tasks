2055786adaaf77ced81089ffad3e8ec14bbcc414
deko possas <andrepossaster@gmail.com>
2021-07-28 16:45:22 -0300

    format

--- frontend/src/utils/routes/index.tsx
+++ frontend/src/utils/routes/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from '../../views/home'
-import Login from '../../views/login'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../../views/home";
+import Login from "../../views/login";
 
 function Routes() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/" component={Login} />
-        <Route path="/home" component={Home} />
-      </Switch>
-    </div>
-  )
+	return (
+		<div>
+			<Switch>
+				<Route path="/" component={Login} />
+				<Route path="/home" component={Home} />
+			</Switch>
+		</div>
+	);
 }
 
-export default Routes
+export default Routes;
