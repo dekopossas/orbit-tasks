f8f94b03dff0f807755c3f306c70f5b586e21a94
deko possas <andrepossaster@gmail.com>
2021-07-28 16:03:41 -0300

    import button remove

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@seidor-cloud-produtos/4tax-react-components';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './utils/routes';
 
