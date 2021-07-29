292bc5ef8153ea0d3714181875723cff7fa7de1e
deko possas <andrepossaster@gmail.com>
2021-07-29 11:47:23 -0300

    login starter

--- frontend/src/views/login/index.tsx
+++ frontend/src/views/login/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import containerStyle from 'assets/css/modules/container.module.css'
 
 function Login() {
+
+  // -------------------------------------------------
+	// Classnames
+	// -------------------------------------------------
+	const { logo } = style;
+	const page = `${style.loginPageContainer} ${style.pageGrid} ${style.backgroundImage}`;
+	const card = `${containerStyle.accessFormCard} ${style.formCard}`;
+	const footer = `${containerStyle.footerSignature} ${style.loginFooter}`;
+
   return (
-    <div>
-      Aqui vai ter um loguin com imputs e buttons
-    </div>
+    <div className={page}>
+			<div className={logo}>
+				<Logo />
+			</div>
+			<div className={card}>
+				{isFirstAccess ? (
+					<FirstAccessForm userCredentials={userCredentials} />
+				) : (
+					<RegularAccessForm
+						setUserCredentials={(values) => onFetchFirstLogin(values)}
+					/>
+				)}
+			</div>
+			<footer className={footer}>
+				Desenvolvido por Seidor Brasil. Conheça nossos serviços em
+				<a href="https://www.seidor.com.br"> www.seidor.com.br</a>
+			</footer>
+		</div>
   )
 }
 
