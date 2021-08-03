23fa4394bbfbfc023fd92dd0d10bb511abc68648
deko possas <andrepossaster@gmail.com>
2021-08-03 16:18:04 -0300

    initial state

---
+++ frontend/src/models/Login/model.ts
@@ -0,0 +1,12 @@
+import { Model, loginRequestResponse, ITenannt } from "./types";
+
+const INITUAL_STATE = {
+  user_id: "user_id",
+  username: "username",
+  name: null,
+  tenants: [],
+};
+
+export default modelFactory<Model>({
+  return 
+});
