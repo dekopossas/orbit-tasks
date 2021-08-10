4347e8ad4a0f6ba4271c75771a68abe5d655d762
deko possas <andrepossaster@gmail.com>
2021-08-10 11:49:56 -0300

    create model cache isolate, have lunch

--- frontend/src/utils/factory/model/index.ts
+++ frontend/src/utils/factory/model/index.ts
@@ -1,4 +1,16 @@
 // Interfaces
 import { Model } from "@rematch/core";
-import Config, {ModelState, genericModel } from "./types";
+import Config, { ModelState, genericModel } from "./types";
 
+// Store
+import store from "../../../store";
+import { handleCache, handleAfter, prepareMock } from "./utils";
+
+export default function createModel<T = genericModel>(config: Config) {
+  // Cache
+  const cacheIndex = handleCache(config, "index");
+  const cacheShow = handleCache(config, "show");
+  const cacheCreate = handleCache(config, "create");
+  const cacheUpdate = handleCache(config, "update");
+  const cacheDelete = handleCache(config, "delete");
+}
