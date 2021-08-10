da20fc92f6791b08b38925339b608a1abe3a94ee
deko possas <andrepossaster@gmail.com>
2021-08-10 10:53:00 -0300

    types of generic object, effect callback and crud

--- frontend/src/utils/factory/model/types.ts
+++ frontend/src/utils/factory/model/types.ts
@@ -1,4 +1,8 @@
+/* eslint-disable prettier/prettier */
+export type genericObject = { [key: string]: genericObject | any };
 export type genericModel = Record<string, unknown>;
+export type effectCallback = (data: genericObject) => string;
+export type crudTypes = "index" | "general" | "show" | "create" | "update" | "delete";
 
 export interface ModelState<T = genericModel> {
   list: {
