9927a92cb6d7920ca42974464c65630f11555349
deko possas <andrepossaster@gmail.com>
2021-08-10 10:28:13 -0300

    types of model state

---
+++ frontend/src/utils/factory/model/types.ts
@@ -0,0 +1,13 @@
+export interface ModelState<T = genericModel> {
+  list: {
+    data: T[];
+    page: number;
+    size: number;
+    sort?: {
+      param: string;
+      order: string;
+    };
+    expand: boolean;
+  };
+  data: T | false;
+}
